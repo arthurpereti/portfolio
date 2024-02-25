f7810fef5a4ab8526c2a7085e2a0c48ee238c5f9
Arthur Pereti <arthurcoelho2005@outlook.com>
2024-02-24 23:03:28 -0300

    atualizações no layout

---
+++ public/js/scrollreveal.js
@@ -0,0 +1,7 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".apresentacaopessoal" ,{ duration:2500 });
+
+ScrollReveal().reveal(".indicator" ,{ duration: 12000 });
+
+ScrollReveal().reveal(".projetostittlecenter, .projetos" ,{ duration: 2000 });
